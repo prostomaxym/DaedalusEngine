e049c6ee82e252693b40677161e036ce310f7907
Maxym Yershykov <ershikov.maksim@gmail.com>
2022-08-13 20:07:30 +0300

    add logging

--- Daedalus/source/Daedalus/EntryPoint.h
+++ Daedalus/source/Daedalus/EntryPoint.h
@@ -6,6 +6,12 @@ extern Daedalus::Application* Daedalus::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Daedalus::Log::Init();
+	DL_CORE_WARN("Initialized core logger;");
+	int a = 5;
+	DL_INFO("Initialized client logger;");
+	DL_INFO("Test var = {0}", a);
+
 	auto app = Daedalus::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Daedalus/source/Daedalus/Log.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+
+#include <spdlog/spdlog.h>
+
+#include "Core.h"
+
+namespace Daedalus {
+
+class DAEDALUS_API Log
+{
+public:
+	static void Init();
+
+	inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return m_core_logger; }
+	inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return m_client_logger; }
+
+private:
+	static std::shared_ptr<spdlog::logger> m_core_logger;
+	static std::shared_ptr<spdlog::logger> m_client_logger;
+};
+
+}
+
+// CORE LOG LEVELS
+#define DL_CORE_TRACE(...)		::Daedalus::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define DL_CORE_INFO(...)		::Daedalus::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define DL_CORE_WARN(...)		::Daedalus::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define DL_CORE_ERROR(...)		::Daedalus::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define DL_CORE_FATAL(...)		::Daedalus::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// CLIENT LOG LEVELS
+#define DL_TRACE(...)			::Daedalus::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define DL_INFO(...)			::Daedalus::Log::GetClientLogger()->info(__VA_ARGS__)
+#define DL_WARN(...)		    ::Daedalus::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define DL_ERROR(...)			::Daedalus::Log::GetClientLogger()->error(__VA_ARGS__)
+#define DL_FATAL(...)			::Daedalus::Log::GetClientLogger()->fatal(__VA_ARGS__)

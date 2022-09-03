017a6033bd9d0e53aedc1238a05bde1725c8befd
Maxym Yershykov <ershikov.maksim@gmail.com>
2022-09-04 02:50:36 +0300

    add high performance gpu request

--- dlSandbox/source/SandboxApp.cpp
+++ dlSandbox/source/SandboxApp.cpp
@@ -1,5 +1,12 @@
 #include <Daedalus.h>
 
+//Force higher performance GPU usage
+extern "C"
+{
+	_declspec(dllexport) DWORD NvOptimusEnablement = 1;
+	_declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
 class Sandbox : public Daedalus::Application
 {
 public:

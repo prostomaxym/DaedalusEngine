e2acacf9ef5611c151f986442e71889a904eafa1
Maxym Yershykov <ershikov.maksim@gmail.com>
2022-09-10 06:02:17 +0300

    fix gpu performance declspec linux

--- dlSandbox/source/SandboxApp.cpp
+++ dlSandbox/source/SandboxApp.cpp
@@ -1,11 +1,13 @@
 #include <Daedalus.h>
 
 //Force higher performance GPU usage
+#ifdef DL_PLATFORM_WINDOWS
 extern "C"
 {
 	_declspec(dllexport) DWORD NvOptimusEnablement = 1;
 	_declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
+#endif
 
 class Sandbox : public Daedalus::Application
 {

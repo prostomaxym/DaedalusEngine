ebf908712360da782f3a21f00483aa1dfcfbaf0e
Maxym Yershykov <ershikov.maksim@gmail.com>
2022-09-10 20:55:48 +0300

    add cmake install

---
+++ WinScripts/CMakeInstall.bat
@@ -0,0 +1,8 @@
+@echo off
+
+cd ../
+cd build
+
+cmake --install . --prefix ../install
+
+PAUSE
---
+++ WinScripts/GenerateCleanProjects.bat
@@ -0,0 +1,15 @@
+@echo off
+
+cd ../
+
+if exist build\ (
+  rmdir build /s /q
+)
+
+if exist build\ (
+  echo Cant delete build folder. Close all projects and retry
+  pause
+) else (
+ cd WinScripts/
+  call GenerateProjects.bat
+)

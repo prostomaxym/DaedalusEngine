3e5334ceee211d4ad4b56206fb6f2511ff9baf93
Maxym Yershykov <ershikov.maksim@gmail.com>
2022-09-13 01:31:06 +0300

    add root rights

--- LinuxScripts/InstallDependencies.sh
+++ LinuxScripts/InstallDependencies.sh
@@ -3,4 +3,4 @@ sudo apt-get install libgl1-mesa-dev
 sudo apt-get install mesa-utils
 sudo apt install libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libxext-dev
 
-./InstallVCPKG.sh
+sudo sh InstallVCPKG.sh

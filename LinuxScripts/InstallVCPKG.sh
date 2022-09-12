0754ef520d528cdb39cd0a2e3a56a6d004314f8e
Maxym Yershykov <ershikov.maksim@gmail.com>
2022-09-13 01:22:44 +0300

    adjust project settings to vcpkg

---
+++ LinuxScripts/InstallVCPKG.sh
@@ -0,0 +1,5 @@
+cd ../
+cd ThirdParty/vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg --triplet=x64-linux install glfw3
+./vcpkg --triplet=x64-linux install gtest
--- WinScripts/InstallVCPKG.ps1
+++ WinScripts/InstallVCPKG.ps1
@@ -2,7 +2,4 @@ cd ../
 cd ThirdParty/vcpkg
 ./bootstrap-vcpkg.bat
 ./vcpkg --triplet=x64-windows install glfw3
-./vcpkg --triplet=x64-windows install spdlog
-./vcpkg --triplet=x64-windows install fmt --v 8.1.1
 ./vcpkg --triplet=x64-windows install gtest
-pause

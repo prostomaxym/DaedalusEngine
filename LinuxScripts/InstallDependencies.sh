f55e132545a32640c25284edd14937cd351e5adf
Maxym Yershykov <ershikov.maksim@gmail.com>
2022-09-13 01:43:59 +0300

    change linux dep

--- LinuxScripts/InstallDependencies.sh
+++ LinuxScripts/InstallDependencies.sh
@@ -2,5 +2,5 @@ sudo apt update
 sudo apt-get install libgl1-mesa-dev
 sudo apt-get install mesa-utils
 sudo apt install libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libxext-dev
-
-sudo sh InstallVCPKG.sh
+sudo apt-get install libglfw3-dev
+sudo apt-get install libgtest-dev

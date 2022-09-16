736f170fdb7192b51dead168d9810f2cac07af95
Maxym Yershykov <ershikov.maksim@gmail.com>
2022-09-16 20:28:44 +0300

    add glad additional features

--- LinuxScripts/InstallVCPKG.sh
+++ LinuxScripts/InstallVCPKG.sh
@@ -3,4 +3,4 @@ cd ThirdParty/vcpkg
 ./bootstrap-vcpkg.sh
 ./vcpkg --triplet=x64-linux install glfw3
 ./vcpkg --triplet=x64-linux install gtest
-./vcpkg --triplet=x64-linux install glad
+./vcpkg --triplet=x64-linux --recurse install glad[gl-api-latest]
--- WinScripts/InstallVCPKG.ps1
+++ WinScripts/InstallVCPKG.ps1
@@ -3,4 +3,4 @@ cd ThirdParty/vcpkg
 ./bootstrap-vcpkg.bat
 ./vcpkg --triplet=x64-windows install glfw3
 ./vcpkg --triplet=x64-windows install gtest
-./vcpkg --triplet=x64-windows install glad
+./vcpkg --triplet=x64-windows --recurse install glad[gl-api-latest]

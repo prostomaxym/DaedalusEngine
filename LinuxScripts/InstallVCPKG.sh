e4de33d202d894b537397371f79e52efd05d1b26
Maxym Yershykov <ershikov.maksim@gmail.com>
2022-09-20 21:29:35 +0300

    add vcpkg json

--- LinuxScripts/InstallVCPKG.sh
+++ LinuxScripts/InstallVCPKG.sh
@@ -1,6 +1,3 @@
 cd ../
 cd ThirdParty/vcpkg
 ./bootstrap-vcpkg.sh
-./vcpkg install glfw3 --triplet=x64-linux
-./vcpkg install gtest --triplet=x64-linux
-./vcpkg install glad[gl-api-latest] --triplet=x64-linux --recurse
--- WinScripts/InstallVCPKG.ps1
+++ WinScripts/InstallVCPKG.ps1
@@ -1,6 +1,2 @@
 cd ../
 cd ThirdParty/vcpkg
-./bootstrap-vcpkg.bat
-./vcpkg install glfw3 --triplet=x64-windows --x-install-root=C:\Users\ershi\source\repos\prostomaxym\Daedalus\ThirdParty\vcpkg\installed
-./vcpkg install gtest --triplet=x64-windows --x-install-root=C:\Users\ershi\source\repos\prostomaxym\Daedalus\ThirdParty\vcpkg\installed
-./vcpkg install glad[gl-api-latest] --triplet=x64-windows --recurse --x-install-root=$(get-location)\..\ThirdParty\vcpkg\installed

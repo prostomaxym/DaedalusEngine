c94696bc2e9e337d7b5e144195aa8aca1ea169ba
Максим Едуардович <ershikov.maksim@gmail.com>
2024-07-13 23:02:41 +0300

    fix geometry shader limited number of invocations

--- source/shader/Shadow.glsl
+++ source/shader/Shadow.glsl
@@ -14,11 +14,16 @@ void main()
 #version 450 core
 #include "LightSSBO.hglsl"
 
-layout(triangles, invocations = 5) in;
+// If number of light sources * cascades exceeds this number - it will not generate shadowmap
+// Right now I will remain this hard coded
+layout(triangles, invocations = 32) in;
 layout(triangle_strip, max_vertices = 3) out;
 
 void main()
 {          
+    if (gl_InvocationID >= ubo_light_proj_view.length())
+        return;
+        
     for (int i = 0; i < 3; ++i)
     {
         gl_Position = ubo_light_proj_view[gl_InvocationID] * gl_in[i].gl_Position;
